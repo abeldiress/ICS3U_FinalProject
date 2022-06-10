b89cfa32a76221c60d880ae82433f9263ed3f7c3
Abel Diress <78807901+abeldiress@users.noreply.github.com>
2022-06-09 21:32:15 -0400

    Add files via upload

--- README.md
+++ README.md
@@ -1,3 +1,75 @@
-# ICS3U_FinalProject
+# Project
+This is my final project for my ICS3U course, a web app for students to help each other with their schoolwork.
 
-This is my final project for my ICS3U course, a web app to help students share resources and ask questions.
+# Features
+The key features of the program:
+- Login/Signup
+- Questions Page - ask questions to everyone signed up
+- Replying to questions - reply to anyone's question
+- Pinning the questions into a permanent questions - keep them for later use
+
+The questions, similar to that of social media videos, get removed at the end of the day. This feature was chosen to focus the users on the most relevant questions, and incentivise those who ask questions to choose theirs carefully.
+
+# Installation
+To use, the following packages must be installed:
+- pyrebase
+- flask
+- requests
+
+The following line can be inputted into your machine's terminal: `pip install pyrebase flask requests`.
+
+# Known Bugs
+There should be no apparent bugs if all the steps are followed.
+
+# Data Structure
+The biggest part of this project in terms of data is the `question` data structure, which has the following:
+
+- `id` - unique identifier for question
+- `author` - name of user who asked question
+- `title` - title of question
+- `question` - body text of question
+- `email` - email of user (stored for bookeeping purposes)
+- `date` - date it was created
+- `time` - time it was created
+- `resource` - boolean value determineg whether the question is part of the resources
+- `replies` - list of replied containg some text, time, and author of reply
+
+# Exaplantion for the Lack of Modularity
+Looking at the code, you might see some cases of reptitive of code, such as:
+`if str(request.cookies.get('email')) == 'None' or str(request.cookies.get('email')) == '': return redirect(url_for('login'))`
+
+Although it might seem intuitive to place thse in a function and then calling such function, this is simply not possible. As all fucntions relating to webpages in Flask are utility functions, that function *must* return something. This is found in more detail here: https://flask.palletsprojects.com/en/2.1.x/api/.
+
+# Docstrings
+In most cases, the args in the docstrings will refer to the inputted HTML in the POST method in which the function is inputted. If not, I will indicate this with '(non-form)' It will also list all possible redirects in the fucntions. Docstrings will have different args/returns for GET and POST when necessary (ex: `login()`).
+
+Also, for all Flaks utility functions, a request object is automatically passed . I mentioned it in each docstring just to acknowledge its presence.
+
+# Support
+If you encounter any unexpected errors or occurences, contact abeldiress05@gmail.com as soon as possible.
+
+# Sources
+List of my sources:
+https://flask.palletsprojects.com/en/2.1.x/quickstart/#a-minimal-application
+Starting code for Flask application.
+
+https://docs.python.org/3/library/venv.html
+Creation of virtual python environment.
+
+https://flask.palletsprojects.com/en/1.1.x/patterns/templateinheritance/
+Template inheritance for HTML file renders in Flask.
+
+https://bulma.io/documentation/overview/start/
+Importing the Bulma CSS framework (frontend).
+
+https://github.com/thisbejim/Pyrebase
+Pyrebase - A simple python wrapper for the Firebase API.
+
+https://firebase.google.com/docs/auth
+Firebase Authentication Documentation.
+
+https://firebase.google.com/docs/database
+Firebase Database Documentation.
+
+https://www.tutorialspoint.com/flask/flask_cookies.htm
+Cookies tutorial in Flask.
--- app.py
+++ app.py
@@ -1,10 +1,18 @@
+"""
+Abel Diress
+ICS3U-04
+Flask app to ask questions about schools and reply.
+"""
+
 from flask import Flask, render_template, request, redirect, url_for, make_response
 from requests import HTTPError
-from datetime import date, datetime
+from datetime import datetime
 import pyrebase
 
+# inital flask app
 app = Flask(__name__)
 
+# configuration settings for database
 config = {
     'apiKey': "AIzaSyA7LKeXKzL7r09GBvpVFx6uje023E2Q6BU",
     'authDomain': "ics-final-project.firebaseapp.com",
@@ -15,113 +23,255 @@ config = {
     'appId': "1:337514158551:web:6cdae623b07b070c9f1392"
 }
 
+# initalized connection to firebase database based on configurations above
 firebase = pyrebase.initialize_app(config)
 auth = firebase.auth()
 db = firebase.database()
 
-def accountInfo(req):
-    if str(req.cookies.get('email')) == 'None' or str(req.cookies.get('email')) == '':
-        return redirect(url_for('login'))
-
+def getUser(req):
+    '''
+    Logs user and retireves authentication data using cookie data.
+    Args:
+        request: werkzeug.local.LocalProxy
+    Returns:
+        user: Pyrebase User
+    '''
+    # gets user cookies and logs them in
     email = req.cookies.get('email')
     password = req.cookies.get('password')
-
     user = auth.sign_in_with_email_and_password(email, password)
 
-    accountInfo = auth.get_account_info(user['idToken'])
-    return db.child('/users/' + accountInfo['users'][0]['localId'] + '/').get(user['idToken'])
+    return user
 
-@app.route('/logout')
+# logout function - destroys login cookies and redirects to login page
+@app.route('/logout', methods=['GET'])
 def logout():
-    request.cookies.pop('email', None)
-    request.cookies.pop('password', None)
-    return redirect(url_for('login'))
+    '''
+    GET: removes cookies from user machine
+    Args:
+        request: werkzeug.local.LocalProxy
+    Returns:
+        login: Response
+    '''
+    resp = make_response(redirect(url_for('login')))
+    resp.set_cookie('email', '')
+    resp.set_cookie('password', '')
+    return resp
 
-# home function
-@app.route('/')
+# home function - displays recent questions
+@app.route('/', methods=['GET'])
 def home():
+    '''
+    GET: displays daily questions and responses
+    Args:
+        request: werkzeug.local.LocalProxy
+    Returns:
+        home: Response
+    '''
+
+    # checks if user is logged in, if not, redirects to login page
     if str(request.cookies.get('email')) == 'None' or str(request.cookies.get('email')) == '':
         return redirect(url_for('login'))
 
-    email = request.cookies.get('email')
-    password = request.cookies.get('password')
+    # gets all questions from database and their ids
+    all_questions = list(dict(db.child('/questions').get().val()).values())
+    question_ids = list(dict(db.child('/questions').get().val()).keys())
 
-    user = auth.sign_in_with_email_and_password(email, password)
+    # merges the question data with its respective id in a dictionary
+    for i in range(len(all_questions)):
+        all_questions[i]['id'] = question_ids[i]
 
-    questions = list(dict(db.child('/questions').get().val()).values())
+    # num_of_questions is the number of questions posted today (not including those in resources)
+    present_questions = []
+    num_of_questions = 0
 
-    for q in questions:
-        if q['date'] == datetime.now().strftime('%d-%m-%y'):
-            questions.remove(q)
-    
+    # sifts through all questions that are not posted today or part of the resources
+    for i in range(len(all_questions)):
+        if all_questions[i]['resource']:
+            present_questions.append(all_questions[i])
+            continue
+
+        if all_questions[i]['date'] == datetime.now().strftime('%d-%m-%y'):
+            num_of_questions += 1
+            present_questions.append(all_questions[i])
+        
+    # date in string format
     expanded_date = datetime.now().strftime('%B %d, 20%y')
-    return render_template('home.html', questions=questions, num_of_questions=len(questions), expanded_date=expanded_date)
+
+    # returns home.html with all the questions, number, and date
+    return render_template('home.html', questions=present_questions, num_of_questions=num_of_questions, expanded_date=expanded_date)
 
 @app.route('/question', methods=['GET', 'POST'])
 def question():
+    '''
+    GET: displays input form for user to enter their question
+    Args:
+        request: werkzeug.local.LocalProxy
+    Returns:
+        question: Response
+    
+    POST: uploads question to db and redirects to home
+    Args:
+        title: str
+        question: str
+    Returns:
+        home: Response
+    '''
     if request.method == 'POST':
+        # extracts the data inputted by the user in the HTML
         title = request.form['title']
         question = request.form['question']        
         
+        # checks if user is logged in, if not, redirects to login page
         if str(request.cookies.get('email')) == 'None' or str(request.cookies.get('email')) == '':
             return redirect(url_for('login'))
 
-        email = request.cookies.get('email')
-        password = request.cookies.get('password')
-        user = auth.sign_in_with_email_and_password(email, password)
+        user = getUser(request)
 
+        # accesses user data from database
         accountInfo = auth.get_account_info(user['idToken'])
         userInfo = dict(db.child('/users/' + accountInfo['users'][0]['localId'] + '/').get(user['idToken']).val())
 
         name = userInfo[list(userInfo.keys())[0]]['name']
         
-        question = {'title': title, 'question': question, 'name': name, 'email': user['email'], 'time': datetime.now().strftime('%H:%M:%S'), 'date': datetime.now().strftime('%d:%m:%y')}
+        # basic question data format
+        question = {'title': title, 'question': question, 'name': name, 'email': user['email'], 'replies': [], 'resource': False, 'time': datetime.now().strftime('%H:%M:%S'), 'date': datetime.now().strftime('%d-%m-%y')}
         
+        # uploads question to database, wiht the root being a unique, random id
         db.child('/questions/').push(question, user['idToken'])
                     
+        # returns to home page
         return redirect(url_for('home'))
     return render_template('question.html')
 
-# verify email function
-@app.route('/verify')
-def verify():
+
+@app.route('/reply/<question_id>', methods=['GET', 'POST'])
+def reply(question_id):
+    '''
+    GET: displays input form for user to enter their reply
+    Args:
+        request: werkzeug.local.LocalProxy
+    Returns:
+        reply: Response
+    
+    POST: uploads reply to db and redirects to home
+    Args:
+        question_id(non-form): str
+        text: str
+    Returns:
+        home: Response
+    '''
+    # checks if user is logged in, if not, redirects to login page
     if str(request.cookies.get('email')) == 'None' or str(request.cookies.get('email')) == '':
         return redirect(url_for('login'))
 
-    email = request.cookies.get('email')
-    password = request.cookies.get('password')
-    user = auth.sign_in_with_email_and_password(email, password)
+    user = getUser(request)
+
+    if request.method == 'POST':
+        # basic reply data format
+        text = request.form['text']
+        reply_response = {'user': user['email'], 'text': text, 'time': datetime.now().strftime('%H:%M:%S')}
+
+        # gets the current list of replies, if none, creates list
+        replies = db.child(f'/questions/{question_id}/replies/').get().val()
+        if replies == None:
+            replies = []
+            
+        # adds reply to list and then updates the database with new reply
+        replies.append(reply_response)
+        db.child(f'/questions/{question_id}/').update({'replies': replies})
+
+        # returns with home page
+        return redirect(url_for('home'))
+    
+    # gets question data
+    question = dict(db.child(f'/questions/{question_id}/').get().val())
+    return render_template('reply.html', question=question)
+
+@app.route('/resource/<question_id>', methods=['GET'])
+def addResource(question_id):
+    '''
+    GET: cahnges question to resource and redirects to home
+    Args:
+        question_id(non-form): str
+        request: werkzeug.local.LocalProxy
+    Returns:
+        home: Response
+    '''
+    # changes question to resource by changing a key in its dict, then redirects to home
+    db.child(f'/questions/{question_id}').update({'resource': True})
+    return redirect(url_for('home'))
+
+# verify email function
+@app.route('/verify', methods=['GET'])
+def verify():
+    '''
+    GET: checks if user verified their email, sends email if not
+    Args:
+        request: werkzeug.local.LocalProxy
+    Returns:
+        home: Response
+    '''
+    # checks if user is logged in, if not, redirects to login page
+    if str(request.cookies.get('email')) == 'None' or str(request.cookies.get('email')) == '':
+        return redirect(url_for('login'))
+
+    user = getUser(request)
+
+    # if not verified, informs user; if not, sends user to home page
     if not auth.get_account_info(user['idToken'])['users'][0]['emailVerified']:
         auth.send_email_verification(user['idToken'])
-        return '<h1 style="font-family: Source Sans Pro;" align="center">Your account has yet to be verified. Check your email to verify this account.</h1>'
+        return '<h1 align="center">Your account has yet to be verified. Check your email to verify this account.</h1>'
     else:
         return redirect(url_for('home'))
 
 @app.route('/signup', methods=['GET', 'POST'])
 def signup():
+    '''
+    GET: displays input form for user to enter their signup info
+    Args:
+        request: werkzeug.local.LocalProxy
+    Returns:
+        verify: Response
+        or
+        signup: Response
+    
+    POST: creates user in db and auth, redirects to verify
+    Args:
+        username: str
+        email: str
+        pwd: str
+        pwd_confirm: str
+    Returns:
+        home: Response
+    '''
     if request.method == 'POST':
+        # gets inputted HTML data
         name = request.form['username']
         email = request.form['email']
         pwd = request.form['pwd']
         pwd_confirm = request.form['pwd_confirm']
 
+        # returns user to page with error if the password and confirm password are not the same
         if pwd != pwd_confirm:
             return render_template('signup.html', confirmFail=True, weakPwd=False)
 
         try:
+            # creates user in Firebase's authentication service and saves the name in the database
             user = auth.create_user_with_email_and_password(email, pwd)
-
-            data = {'name': name, 'email': email, 'grade': 0, 'groups':[]}
+            data = {'name': name, 'email': email}
             
+            # pushes user data into database with key being idToken
             accountInfo = auth.get_account_info(user['idToken'])
             db.child('/users/' + accountInfo['users'][0]['localId'] + '/').push(data, user['idToken'])
 
+            # stores cookies and redirects to verify page
             resp = make_response(redirect(url_for('verify')))
             resp.set_cookie('email', email)
             resp.set_cookie('password', pwd)
             return resp
         except Exception as err:
-            print(err)
+            # if password does not satisfy Firebase's built-in password rules, returns to page w/ error
             if 'WEAK_PASSWORD' in str(err):
                 return render_template('signup.html', confirmFail=False, weakPwd=True)
 
@@ -129,19 +279,39 @@ def signup():
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
+    '''
+    GET: displays input form for user to enter their login info
+    Args:
+        request: werkzeug.local.LocalProxy
+    Returns:
+        home: Response
+        or
+        login: Response
+    
+    POST: logs user in and sends to verify
+    Args:
+        email: str
+        pwd: str
+    Returns:
+        verify: Response
+    '''
+    
     if request.method == 'POST':
+        # get inputted user data
         email = request.form['email']
         pwd = request.form['pwd']
         try:
+            # verifies if login credntials are valid, if not, it will be caught by the try, except
             user = auth.sign_in_with_email_and_password(email, pwd)
-            accountInfo = auth.get_account_info(user['idToken'])
 
+            # saves user cookies
             resp = make_response(redirect(url_for('verify')))
             resp.set_cookie('email', email)
             resp.set_cookie('password', pwd)
             return resp
         except HTTPError as e:
             e = str(e)
+            # returns user to same apge with text based on the error
             if 'EMAIL_NOT_FOUND' in e:
                 return render_template('login.html', invalidPwd=True, content='This account doesn\'t exist. Try a different email or create an account.')
             elif 'INVALID_PASSWORD' in e:
@@ -150,14 +320,13 @@ def login():
                 return render_template('login.html', invalidPwd=True, content='Too many unsuccessful login attempts. Please try again later.')
                     
         return redirect(url_for('verify'))
-        
+    
+    # if user is already logged in, redirect to home page, if not, render the login HTML
     try:
-        email = request.cookies.get('email')
-        password = request.cookies.get('password')
-        user = auth.sign_in_with_email_and_password(email, password)
+        user = getUser(request)
         return redirect(url_for('home'))
     except:
         return render_template('login.html', invalidPwd=False)
 
-
+# runs all the routing functions on a WSGI web app on port 4000
 app.run(port=4000, debug=True)
